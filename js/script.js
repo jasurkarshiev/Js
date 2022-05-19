c82cf32a851e14d2cd708a7d58f6e88f26b5c27e
jasurkarshiev <jasurms7171@gmail.com>
2022-05-19 23:59:15 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// const headerBtn = document.querySelector (".header__btn");
+// const elHeader = document.querySelector('.header')
+// let headerOpen = false;
+
+
+// headerBtn.addEventListener("click", () => {
+//    if(!headerOpen) {
+//       headerBtn.classList.add('open');
+//       elHeader.classList.add('open-header');
+//       headerOpen = true;
+//    } else {
+//       headerBtn.classList.remove('open');
+//       elHeader.classList.remove('open-header');
+//       headerOpen = false;
+//    }
+// });
+var hello = alert("Здраствуйте");
+var greetings = alert("Добро Пожаловать");
+var UserName = prompt("Введите своё имя !", "Жасур");
+var SureName = prompt("Введите свою Фамилию !", "Каршиев");
+var Age = prompt("Введите свой возраст !", "22");
+var Study = prompt("Где вы Учитесь ?", "Najot Ta'lim");
+var isMarried = prompt("Вы женаты ?", "Нет");
+var doYouKnowComputerProgramm = ("Умеете ли вы программировать ?", "пока что нет");
+var aboutYou = prompt("Красткое Интервью про себя", "I am a beginner web designer.");
+
+console.log("hello");
+console.log("greetings");
+console.log("UserName");
+console.log("SureName");
+console.log("Age");
+console.log("Study");
+console.log("isMArried");
+console.log("doYouKnowComputerProgramm");
+console.log("aboutYou");
